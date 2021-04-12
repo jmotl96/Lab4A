bc9f2cc5daf0247b5aafbf0eee320119d7a38ec8
jmotl96 <jmotl96@gmail.com>
2021-04-12 14:37:51 -0500

    Initial commit

---
+++ Lab4A/Lab4A/MainPage.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Lab4A
+{
+    public partial class MainPage : ContentPage 
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private String morcein = "";
+        private String word = "";
+        
+
+        private void AddADot(object sender, EventArgs e)
+        {
+            morcein += ".";
+            dot.Text = morcein;
+        }
+
+        private void addDassh(object sender, EventArgs e)
+        {
+            morcein += "-";
+            dot.Text = morcein;
+        }
+
+        private void Space(object sender, EventArgs e)
+        {
+            word +=  Morse.MorseCoder(morcein);
+            Output.Text = word;
+            morcein = "";
+            dot.Text = morcein;
+        }
+    }
+}
